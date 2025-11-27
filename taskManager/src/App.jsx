5ee724f4cd1a96ee6b5b15b105b3102c005bc507
Swapnil Bhalekar <swapnilvbhalekar@gmail.com>
2025-11-28 00:11:13 +0530

    Add new project: myNewReactApp

---
+++ taskManager/src/App.jsx
@@ -0,0 +1,13 @@
+import NewProject from "./component/NewProject";
+import SideBar from "./component/sideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar />
+      <NewProject />
+    </main>
+  );
+}
+
+export default App;
