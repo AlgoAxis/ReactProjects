69de410dbd88fc2c655f46f104f179aadf3b993a
Swapnil Bhalekar <swapnilvbhalekar@gmail.com>
2025-11-29 02:18:57 +0530

    added selectedProject component with displaying the input with style

--- taskManager/src/App.jsx
+++ taskManager/src/App.jsx
@@ -1,12 +1,74 @@
+import { useState } from "react";
 import NewProject from "./component/NewProject";
 import NoProjectSelected from "./component/NoProjectSelected";
-import SideBar from "./component/sideBar";
+import SideBar from "./component/SideBar";
+import SelectedProject from "./component/SelectedProject";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId : undefined,
+    projects : []
+  });
+
+  function handleSelectProject(id){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId : id,
+      };
+    });
+  }
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId : null
+      }
+    })
+  }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects : [...prevState.projects,newProject]
+      }
+    })
+  }
+
+  function handleCancelAddProject(){
+    setProjectsState(prevState => {
+    return {
+        ...prevState,
+        selectedProjectId: undefined,
+      }
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject}/>;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
+  }else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NoProjectSelected />
+      <SideBar 
+      onStartAddProject={handleStartAddProject} 
+      projects={projectsState.projects}
+      onSelectProject={handleSelectProject}
+      selectedProjectId={projectsState.selectedProjectId}
+      />
+      {content}
     </main>
   );
 }
---
+++ taskManager/src/component/SelectedProject.jsx
@@ -0,0 +1,25 @@
+export default function SelectedProject({project}){
+    if (!project) {
+        return <div className="w-[35rem] mt-16">No project selected</div>;
+    }
+
+    const formattedDate = project.dueDate
+      ? new Date(project.dueDate).toLocaleDateString('en-US',{
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric'
+        })
+      : "No due date";
+
+    return <div className="w-[35rem] mt-16">
+        <header className="pb-4 mb-4 border-b-2 border-stone-300">
+            <div className="flex items-center justify-between">
+                <h2 className="text-2xl font-bold text-stone-700 mb-4">{project.title}</h2>
+                <button className="text-stone-700 hover:text-red-500">Delete</button>
+            </div>
+            <p className="mb-4 text-stone-400">{formattedDate}</p>
+            <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
+        </header>
+        TASKS
+    </div>
+}
